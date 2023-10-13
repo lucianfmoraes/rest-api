7b8817ffef228b9d3f1524eab18816680a8103a6
Dieimp <mraingue@gmail.com>
2023-10-13 19:01:57 -0300

    Adequando Entidades

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,4 +1,4 @@
-import { BeforeInsert, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { BeforeInsert, BeforeUpdate, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { Pedido } from 'src/pedido/pedido.entity';
 import { CreateUserInput } from './dto/create-user.input';
 
@@ -45,9 +45,11 @@ export class User{
     pedidos?: Pedido[];
 
     @BeforeInsert()
+    @BeforeUpdate()
     public async hashPassword() {
-      this.password = await this.password.toUpperCase();
+      this.password = await hash(this.password, 10);
     }
+
 
     public toDomain(): CreateUserInput {
         let createUserInput: CreateUserInput = {
